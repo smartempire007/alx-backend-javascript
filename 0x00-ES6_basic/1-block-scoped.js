596e6b1cbad36f0acc374d923b6cdb19fa31ee7a
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-06-28 22:07:08 +0100

    committing 2-arrow.js task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,7 +5,7 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
   if (trueOrFalse) {
     const task = true;
-    const task2 = false; 
+    const task2 = false;
   }
 
   return [task, task2];
