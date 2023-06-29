4104bc25dd0320a596323146510cbb5738ff5a4c
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-06-29 22:17:10 +0100

    new task committed

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
