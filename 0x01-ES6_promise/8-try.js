e5a48c4567fdfbeb37accc4e8455729614f3f84d
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-06-30 01:14:03 +0100

    fixing task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw new Error('Division by zero');
+    throw new Error('cannot divide by 0');
   }
   return numerator / denominator;
 }
