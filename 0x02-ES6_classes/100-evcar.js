de4f2fcc447ab2ea69e2f157dde3fd45aa610f18
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-06-30 04:04:38 +0100

    fix on task 11

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,4 @@
-/* eslint-disable class-methods-use-this */
-import Car from './10-car';
-
+/* eslint-disable */
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
