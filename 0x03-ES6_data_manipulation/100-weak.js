2eafe9db90d318371f7b14b0dd0a6d11218cacf3
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-05 04:39:50 +0100

    fix on task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,7 @@
 export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
+// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
+export function queryAPI(endpoint) {
   let called = 0;
   if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
   weakMap.set(endpoint, called + 1);
