5a25f0f2b2664eb0497e850836d2f46ad5f7624a
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-05 04:06:04 +0100

    fix on task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 /* eslint-disable array-callback-return */
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string') {
+  if (startString === undefined || !startString.length === 0) {
     return '';
   }
   return [...set]
-    .filter((str) => str.startsWith(startString))
-    .map((str) => str.slice(startString.length))
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
     .join('-');
 }
