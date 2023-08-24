6923ac4bcce588f270dc8b2fafe9fb80d857d87d
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-08-24 23:17:34 +0100

    fix on task 07

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,29 +1,54 @@
+/**
+ * Simple express server that handles / and /students routes
+ */
+const fs = require('fs');
 const express = require('express');
-const students = require('./3-read_file_async');
-const app = express();
-const hostname = '127.0.0.1';
+
+const host = '127.0.0.1';
 const port = 1245;
+const app = express();
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+app.get('/students', (req, res) => {
+  const db = process.argv[2] === undefined ? '' : process.argv[2];
+  fs.readFile(db, 'utf-8', (error, data) => {
+    const body = ['This is the list of our students'];
+    if (error) {
+      body.push('Cannot load the database');
+      res.send(body.join('\n'));
+    } else {
+      const courses = new Map();
+      let students = data.split('\n');
+      students = students.slice(1, students.length - 1);
+
+      // Parse CSV data creating a map of courseData objects.
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const firstName = studentData[0];
+        const field = studentData[3];
+        if (courses.has(field)) {
+          courses.get(field).students.push(firstName);
+          courses.get(field).count += 1;
+        } else {
+          courses.set(field, { students: [firstName], count: 1 });
+        }
+      });
+
+      // Organize data in an array
+      body.push(`Number of students: ${students.length}`);
+      courses.forEach((courseData, course) => {
+        body.push(`Number of students in ${course}: ${courseData.count}. List: ${courseData.students.join(', ')}`);
+      });
+      res.send(body.join('\n'));
+    }
+  });
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server is live, running at http://${host}:${port}`);
 });
+
+module.exports = app;
